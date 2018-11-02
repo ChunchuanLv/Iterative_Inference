1a944952694449a050c51f07d3197c2f8a9145f4
ChunchuanLv <chunchuan.lv@gmail.com>
2018-11-02 21:46:24 +0000

    pure elmo added

--- myallennlp/models/__init__.py
+++ myallennlp/models/__init__.py
@@ -3,5 +3,6 @@ These submodules contain the classes for AllenNLP models,
 all of which are subclasses of :class:`~allennlp.models.model.Model`.
 """
 
+from myallennlp.models.elmo_biaffine_dependency_parser import ELMOBiaffineDependencyParser
 from myallennlp.models.full_variational_biaffine_dependency_parser import FullVariationalBiaffineDependencyParser
 from myallennlp.models.variational_iterative_biaffine_dependency_parser import VariationalIterativeBiaffineDependencyParser
